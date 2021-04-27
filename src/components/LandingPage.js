b7afc3f684bcc14ea2807ffcef918d54e025827d
Manuel Remer <manuelremer@web.de>
2021-04-27 17:22:00 +0200

    Style header, image, text

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,10 +4,18 @@ export default function LandingPage() {
   return (
     <div className="landing-page">
       <header className="landing-header">
-        <h1 className="leanding-header-h1">Breadzilla</h1>
+        <h1 className="landing-header-h1">Breadzilla</h1>
       </header>
-      <img alt="crispy fresh bread" src={LandingBread} />
-      <p>Create your own Bread with the ingrediants you want</p>
+      <img
+        alt="crispy fresh bread"
+        src={LandingBread}
+        className="landing-image"
+      />
+      <div className="landing-text">
+        <p>Design your own</p>
+        <p className="landing-text-highlight">Bread</p>
+        <p>with the ingrediants</p> <p>you want</p>
+      </div>
       <nav className="LandingNav">
         <button>Bread generator</button>
         <button>Browse your recipes</button>
