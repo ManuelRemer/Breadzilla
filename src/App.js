01e138ebc06e05ff95fce209ec49ff6c3fa20e54
Manuel Remer <manuelremer@web.de>
2021-04-27 12:32:18 +0200

    Basic setup, install react-router-dom, link fonts in index.html, colors.css, basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route exact path="/generator"></Route>
+          <Route exact path="/recipe"></Route>
+          <Route exact path="/saved-recipe"></Route>
+          <Route exact path="/collection"></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
