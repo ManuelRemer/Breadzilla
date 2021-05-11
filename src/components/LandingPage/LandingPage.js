d9eb30c6c5df40f2125f94168309bb4ae9665290
Manuel Remer <manuelremer@web.de>
2021-05-11 11:27:23 +0200

    Devide LandingPage into Sections

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,27 +1,9 @@
-import LandingBreadSrc from "../../images/LandingBread.jpg";
-import LandingNavigation from "./LandingNavigation/LandingNavigation";
-import Headline from "../StaticTextBoxes/LandingPage/Headline/Headline";
-import Welcome from "../StaticTextBoxes/LandingPage/Welcome/Welcome";
 import "./LandingPage.css";
+import LandingSection from "./LandingSection/LandingSection";
 export default function LandingPage() {
   return (
-    <div className="landing-page">
-      <Headline className="landing-headline" />
-      <img
-        alt="crispy fresh bread"
-        src={LandingBreadSrc}
-        className="landing-image"
-      />
-      <div className="landing-rest">
-        <Welcome />
-
-        <LandingNavigation
-          labels={["Bread generator", "Browse your recipes"]}
-          size="xlarge"
-          routes={["/generator", "/collection"]}
-          action="route"
-        />
-      </div>
+    <div>
+      <LandingSection />
     </div>
   );
 }
---
+++ src/components/LandingPage/LandingSection/LandingSection.js
@@ -0,0 +1,27 @@
+import LandingBreadSrc from "../../../images/LandingBread.jpg";
+import LandingNavigation from "../LandingNavigation/LandingNavigation";
+import Headline from "../../StaticTextBoxes/LandingPage/Headline/Headline";
+import Welcome from "../../StaticTextBoxes/LandingPage/Welcome/Welcome";
+import "../LandingPage.css";
+export default function LandingSection() {
+  return (
+    <div className="landing-page">
+      <Headline className="landing-headline" />
+      <img
+        alt="crispy fresh bread"
+        src={LandingBreadSrc}
+        className="landing-image"
+      />
+      <div className="landing-rest">
+        <Welcome />
+
+        <LandingNavigation
+          labels={["Bread generator", "Browse your recipes"]}
+          size="xlarge"
+          routes={["/generator", "/collection"]}
+          action="route"
+        />
+      </div>
+    </div>
+  );
+}
